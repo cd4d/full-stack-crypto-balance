309bb1a7a428d1e8f60511db77bc9c8d4ce791b2
cd4d <11580810+cd4d@users.noreply.github.com>
2021-10-07 14:26:59 -0400

    Heroku settings 9

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
