4a72ade066eb380a7771cd8deab3d4a14d6f269f
cd4d <11580810+cd4d@users.noreply.github.com>
2021-10-07 14:16:15 -0400

    Heroku settings 8

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -22,7 +22,7 @@ if dotenv_file:
     load_dotenv()
 
 # Root directory
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
