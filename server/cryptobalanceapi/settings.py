cd026876a52861355d6c707aac3c066454683a79
cd4d <11580810+cd4d@users.noreply.github.com>
2021-10-07 18:24:38 -0400

    django react static 1

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = 'cryptobalanceapi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '/build')],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
