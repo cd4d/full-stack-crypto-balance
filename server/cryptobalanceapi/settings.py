ba7233b3f5e46dee782897cea3a505f584c966d1
cd4d <11580810+cd4d@users.noreply.github.com>
2021-10-10 18:53:11 -0400

    Changed to build/statidfiles

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -201,6 +201,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build/staticfiles')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
