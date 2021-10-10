5670da9cc1f636444e0be7b1a53065f74b4f4176
cd4d <11580810+cd4d@users.noreply.github.com>
2021-10-10 18:46:33 -0400

    Changed static to staticfiles static_root settings.py

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -203,4 +203,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
