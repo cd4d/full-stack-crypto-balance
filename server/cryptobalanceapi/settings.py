7c60330b37b84cb0b4c8c3e45e1d332053536cca
cd4d <11580810+cd4d@users.noreply.github.com>
2021-10-11 22:32:21 -0400

    Changed staticfiles_dir to build

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -201,6 +201,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
