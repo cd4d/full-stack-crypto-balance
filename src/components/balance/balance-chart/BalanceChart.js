de1c351ff63bab217698cf8cda2a250a24a65e5c
cd4d <11580810+cd4d@users.noreply.github.com>
2021-11-14 10:42:17 -0500

    [Fix] Re added balance total

--- src/components/balance/balance-chart/BalanceChart.js
+++ src/components/balance/balance-chart/BalanceChart.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Chart } from "primereact/chart";
 import { useSelector, useDispatch } from "react-redux";
 import { balanceActions } from "../../../store/balance-slice";
+import { formatCurrency } from "../../../utils/utils";
+import CurrencyContext from "../../../store/currency-context";
 const BalanceChart = React.memo(() => {
   const dispatch = useDispatch();
   const balance = useSelector((state) => state.balanceReducer.balance);
@@ -11,6 +13,8 @@ const BalanceChart = React.memo(() => {
   const formattedData = useSelector(
     (state) => state.balanceReducer.formattedData
   );
+  const currencyCtx = useContext(CurrencyContext);
+  const total = useSelector((state) => state.balanceReducer.total);
 
   useEffect(() => {
     dispatch(balanceActions.formatLocalData());
@@ -31,13 +35,16 @@ const BalanceChart = React.memo(() => {
   return (
     <>
       {!!balance.length && (
-        <Chart
-          type="doughnut"
-          // need to pass a copy
-          data={{ ...formattedData }}
-          options={chartOptions}
-          style={isBalanceLoading ? { display: "none" } : {}}
-        />
+        <>
+          <h4>Total: {formatCurrency(total, currencyCtx)}</h4>
+          <Chart
+            type="doughnut"
+            // need to pass a copy
+            data={{ ...formattedData }}
+            options={chartOptions}
+            style={isBalanceLoading ? { display: "none" } : {}}
+          />
+        </>
       )}
     </>
   );
