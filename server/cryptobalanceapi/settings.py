b766dc39bd5ed218824fef8f42a34292fb82336f
cd4d <camera8@pm.me>
2023-02-09 14:39:25 +0100

    Db url from env

--- server/cryptobalanceapi/settings.py
+++ server/cryptobalanceapi/settings.py
@@ -111,8 +111,7 @@ WSGI_APPLICATION = 'cryptobalanceapi.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/full-stack-cryptobalance',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )}
 
